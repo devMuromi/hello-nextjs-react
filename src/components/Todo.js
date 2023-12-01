c5a1a08881bbf43ea04c0bdbecef16b138582307
devMuromi <devmuromi@gmail.com>
2023-12-02 05:15:52 +0900

    handling events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,19 @@
 function Todo(props) {
   // props is a special variable that React will pass to our function
   // function always start with capital letter(to differentiate from html tags)
+  function deleteHandler() {
+    console.log("Clicked!");
+    console.log(props.text);
+  }
+
   return (
     <div>
       <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn">Delete</button>
+          <button className="btn" onClick={deleteHandler}>
+            Delete
+          </button>
         </div>
       </div>
     </div>
