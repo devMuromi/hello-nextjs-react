dc24ed2831ff84c0e69422406f56a5adb0034f4c
devMuromi <devmuromi@gmail.com>
2023-12-11 14:45:35 +0900

    update state based on original state

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -7,6 +7,23 @@ const FavoritesContext = createContext({
 
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    // 상태가 기존 상태에 의존적이라면 함수형 업데이트를 사용하는 것이 좋음
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId); // true 반환하면 배열에 남음
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId); // array에 조건을 만족하는 어떤 항목이 있다면 true 반환
+  }
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
